ed7a105154d4f3ad8f16974f6bcdb9e370f9cd53
Tkach Eugene <tkach.yevhenii@chnu.edu.ua>
2022-05-15 17:04:26 +0300

    last changes

--- js/app.js
+++ js/app.js
@@ -7,5 +7,45 @@ $(document).ready(function () {
 	$('.Rew__all__slider').slick({
 
 	});
+
+
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH = intro.innerHeight();
+	let scrollPosition = $(window).scrollTop();
+
+	$(window).on("scroll load", function () {
+		scrollPosition = $(this).scrollTop();
+
+		if(scrollPosition > introH) 
+		{
+			header.addClass("fixed");
+		}
+		else 
+		{
+			header.removeClass("fixed");
+		}
+
+
+		console.log(scrollPosition);
+	});
+
+
+
+
+	$("[data-scroll]").on("click", function(event) {
+		event.preventDefault();
+
+		let elemetnID = $(this).data('scroll')
+		let elemetnOffset = $(elemetnID).offset().top;
+		
+;
+
+		$("html, body").animate({
+			scrollTop:elemetnOffset -100
+		}, 1000);
+	});
+
+
 });
 
